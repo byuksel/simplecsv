d9f526b7aad4045c16d8796faabfd08a5dd36c01
by99 <yoga>
2015-06-21 17:45:40 -0400

    Added tests for parseString() and getArg().\n Added stub action

--- test/csvtest.js
+++ test/csvtest.js
@@ -12,9 +12,38 @@ var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
 var csv = new csv();
-describe('fish.csv browserified tests', function() {
+describe('SimpleCsv.js Unit Test', function() {
 
-  it('Should pass test.csv', function() {
+  it('(getArg) should work', function() {
+    // Create a stub for csv.parseStringToArray
+    var argdic = { hasHeaders: true , movement: 'yes' };
+    expect(csv.getArg(argdic, 'hasHeaders', false)).to.be.true;
+    expect(csv.getArg(argdic, 'myflag', false)).to.be.false;
+    expect(csv.getArg(argdic, 'movement', 'no')).equal('yes');
+    expect(csv.getArg(argdic, 'movement', 1)).equal(1);
+  });
+  
+  it('(parseString) should handle hasHeaders correctly', sinon.test(function() {
+    // Create a stub for csv.parseStringToArray
+    var mystub = this.stub(csv, 'parseStringToArray').returns([['1', '2'], ['3', '4']]);
+    var argdic = { hasHeaders: true };
+    var realOutput = csv.parseString('', argdic);
+    var expectedOutput = csv.makeCsvdataFromObj({ columnNames : [ '1', '2' ],
+                                                  rows : [ [ '3', '4' ] ],
+                                                  rowCount : 1 ,
+                                                  columnCount: 2 });
+    expect(realOutput).to.eql(expectedOutput);
+
+    mystub.returns([['1', '2'], ['3', '4']]);
+    argdic = { hasHeaders: false };
+    realOutput = csv.parseString('', argdic);
+    expectedOutput = csv.makeCsvdataFromObj({ rows : [ [ '1', '2' ], [ '3', '4' ] ],
+                                              rowCount : 2 ,
+                                              columnCount: 2 });
+    expect(realOutput).to.eql(expectedOutput);
+   }));
+
+  it('(parseStringToArray) should parse csv cases in testcases_as_csv.json', function() {
     // Read the expected values from parsedtestJSON
     var expectedResults = {};
     var parsedcases = parsedtestJSON.parsedcases;
